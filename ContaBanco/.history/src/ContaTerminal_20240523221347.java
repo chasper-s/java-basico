646b3bd101c34f98cf59d7f5d5f72e540a05ea8b
Chasper <francajasper@outlook.com>
2024-05-23 22:28:29 -0300

    projeto conta banco

--- ContaBanco/.history/src/ContaTerminal_20240523221347.java
+++ ContaBanco/.history/src/ContaTerminal_20240523221347.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-       
         System.out.println("Por favor, digite o número da Agência:");
         String agencia = scanner.nextLine();
 
@@ -16,21 +13,9 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o nome do Cliente:");
         String nomeCliente = scanner.nextLine();
 
-        double saldo = 0;
-        boolean saldoValido = false;
-        
-        while (!saldoValido) {
-            try {
-                System.out.println("Por favor, digite o saldo da Conta:");
-                saldo = scanner.nextDouble();
-                saldoValido = true;
-            } catch (InputMismatchException e) {
-                System.out.println("Valor de saldo inválido. Por favor, digite um número decimal válido.");
-                scanner.next();  
-            }
-        }
+        System.out.println("Por favor, digite o saldo da Conta:");
+        double saldo = scanner.nextDouble();
 
-        
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + 
                             agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
